42122348f5db2b5abf36b35e71779dff2c0e606c
Preston Van Loon <preston@prysmaticlabs.com>
2019-03-18 11:46:53 -0400

    Revert "Make the testnet site match the main site more"

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -12,7 +11,6 @@ import { HomeComponent } from './home.component';
     CommonModule,
     MatIconModule,
     MatListModule,
-    MatCardModule,
   ]
 })
 export class HomeModule { }
